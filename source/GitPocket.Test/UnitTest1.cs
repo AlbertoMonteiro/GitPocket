f20f80b1ccd49d6d06fc9567076121042f62d961
NPadrutt <nino.padrutt@hotmail.com>
2015-01-15 21:25:55 +0100

    add different basic stuff
    - business project
    - test projects
    - mvvm light
    - octokit
    - bugsense
    - basic for translations

---
+++ source/GitPocket.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GitPocket.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void HelloWorldTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}

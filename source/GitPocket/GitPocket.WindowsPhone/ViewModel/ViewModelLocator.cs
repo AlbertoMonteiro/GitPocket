701d2956ada55b578706b527f0bdeb9b9ff0b12c
NPadrutt <nino.padrutt@hotmail.com>
2015-01-15 22:58:10 +0100

    removes unused template stuff

--- source/GitPocket/GitPocket.WindowsPhone/ViewModel/ViewModelLocator.cs
+++ source/GitPocket/GitPocket.WindowsPhone/ViewModel/ViewModelLocator.cs
@@ -1,32 +1,10 @@
-/*
-  In App.xaml:
-  <Application.Resources>
-      <vm:ViewModelLocator xmlns:vm="clr-namespace:GitPocket"
-                           x:Key="Locator" />
-  </Application.Resources>
-  
-  In the View:
-  DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
-
-  You can also use Blend to do all this with the tool's support.
-  See http://www.galasoft.ch/mvvm
-*/
-
-using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
 using Microsoft.Practices.ServiceLocation;
 
 namespace GitPocket.ViewModel
 {
-    /// <summary>
-    /// This class contains static references to all the view models in the
-    /// application and provides an entry point for the bindings.
-    /// </summary>
     public class ViewModelLocator
     {
-        /// <summary>
-        /// Initializes a new instance of the ViewModelLocator class.
-        /// </summary>
         public ViewModelLocator()
         {
             ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
@@ -42,15 +20,7 @@ namespace GitPocket.ViewModel
             ////    SimpleIoc.Default.Register<IDataService, DataService>();
             ////}
 
-            SimpleIoc.Default.Register<MainViewModel>();
-        }
-
-        public MainViewModel Main
-        {
-            get
-            {
-                return ServiceLocator.Current.GetInstance<MainViewModel>();
-            }
+            //SimpleIoc.Default.Register<MainViewModel>();
         }
         
         public static void Cleanup()

87482f7bac47b7792c5db9be40e645aaab516948
Alberto Monteiro <alberto.monteiro@live.com>
2015-01-16 19:40:49 -0300

    Ninject setup done Ninject setup done Fixes #5

---
+++ source/GitPocket/GitPocket.Shared/ViewModel/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace GitPocket.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        string userName;
+
+        public MainViewModel()
+        {
+            if (IsInDesignMode || IsInDesignModeStatic)
+            {
+                UserName = "email@email.com";
+            }
+            else
+            {
+                UserName = "empty";                
+            }
+        }
+
+        public string UserName
+        {
+            get { return userName; }
+            set
+            {
+                userName = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+}

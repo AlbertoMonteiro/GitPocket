bdefe6e8aa2e266cce24e386562d849b424b1171
Alberto Monteiro <alberto.monteiro@live.com>
2015-01-15 23:07:47 -0300

    Added resourses to Portuguese language

--- source/GitPocket/GitPocket.WindowsPhone/Properties/AssemblyInfo.cs
+++ source/GitPocket/GitPocket.WindowsPhone/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GitPocket.WindowsPhone")]
+[assembly: AssemblyTitle("GitPocket")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GitPocket.WindowsPhone")]
+[assembly: AssemblyProduct("GitPocket")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

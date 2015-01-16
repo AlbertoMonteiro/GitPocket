5bfbe117be761ce6d60c604373945c4457ee4198
Alberto Monteiro <alberto.monteiro@live.com>
2015-01-16 15:05:19 -0300

    Added custom tool to automatizete addition of new keys from en resource to another translations

---
+++ tools/ResourceAutomater/ResourceAutomation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace ResourceAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stringsPath = @"C:\Projetos\GitPocket\source\GitPocket\GitPocket.Shared\Strings";
+            if (args.Length > 0)
+                stringsPath = args[0];
+
+            var serializer = new XmlSerializer(typeof(Root));
+
+            var allResources = Directory.EnumerateFiles(stringsPath, "*.resw", SearchOption.AllDirectories)
+                                        .OrderByDescending(x => x.EndsWith(@"en\Resources.resw"))
+                                        .Select(path => serializer.Deserialize<Root>(path).WithPath(path))
+                                        .ToList();
+
+            var enResource = allResources.First();
+            allResources.Skip(1)
+                .ToList()
+                .ForEach(root => serializer.Serialize(root.WithNewDatas(enResource.Datas), root.Path));
+            
+            Console.WriteLine("Done");
+        }
+    }
+}

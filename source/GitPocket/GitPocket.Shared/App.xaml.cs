b57c9ba7ff7e2f96eb5035729a5656810e906b13
RandomlyKnighted <thughes@randomlyknighted.com>
2015-01-17 00:07:53 -0600

    Disabled frame rate counter for emulator

--- source/GitPocket/GitPocket.Shared/App.xaml.cs
+++ source/GitPocket/GitPocket.Shared/App.xaml.cs
@@ -50,7 +50,7 @@ namespace GitPocket
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
-                this.DebugSettings.EnableFrameRateCounter = true;
+                //this.DebugSettings.EnableFrameRateCounter = true;
             }
 #endif
 

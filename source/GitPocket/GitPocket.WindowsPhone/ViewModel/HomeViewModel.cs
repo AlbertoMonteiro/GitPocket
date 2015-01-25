5fca344d6f4567d4b1bd1d52c81a30c8237f6e4e
RandomlyKnighted <thughes@randomlyknighted.com>
2015-01-24 21:50:17 -0600

    Added octicons nuget package

---
+++ source/GitPocket/GitPocket.WindowsPhone/ViewModel/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GitPocket.Business;
+using GitPocket.ViewModel;
+
+namespace GitPocket.ViewModel
+{
+    public class HomeViewModel : ViewModelBase
+    {
+
+    }
+}
